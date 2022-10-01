c5bc8dd789c30f005c4832a73280c8fc904e0455
swah96 <98216049+swah96@users.noreply.github.com>
2022-10-01 10:25:09 +0100

    Add files via upload

---
+++ sap.js
@@ -0,0 +1,26 @@
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
+    })
+}
+
+async function rainbow() {
+    await delayedColorChange('red', 1000)
+    await delayedColorChange('orange', 1000)
+    await delayedColorChange('yellow', 1000)
+    await delayedColorChange('green', 1000)
+    await delayedColorChange('blue', 1000)
+    await delayedColorChange('indigo', 1000)
+    await delayedColorChange('violet', 1000)
+    return "ALL DONE!"
+}
+
+async function printRainbow() {
+    await rainbow();
+    console.log("END OF RAINBOW!")
+};
+
+printRainbow();
